681cee070a30abf7169831c65474d8777bc3dbde
pagioro <pagioro@hotmail.com>
2022-04-26 08:53:46 +0000

    Improving validate data function

--- run.py
+++ run.py
@@ -26,33 +26,33 @@ def get_users_data():
     print('Please fill in the requested data!\n')  
 
     user_name = input('Enter your name: ')
-    print(user_name)
+
     user_email = input('\nEnter your email: ')
-    print(user_email)
+    validate_data(0,user_email)
+
     user_age = input('\nEnter your age: ')
     print(user_age)
+
     user_gender = input('\nEnter your gender (M or F): ')
     validate_data(1,user_gender)
 
     user_country = input('\nEnter your country: ')
-    print(user_country)
     user_city = input('\nEnter your city: ')
-    print(user_city)
 
     user_rent = input('\nDo you pay rent? (Y or N):  ')
     validate_data(2,user_rent)
 
     user_children = input('\nHow many children do you have? (0 for None):  ')
-    print(user_children)
 
 def validate_data(value1,value2):
     try:
+        if ((value1 == 0) and ('@' not in value2)):
+            raise ValueError('@ required') 
         if ((value1 == 1) and (value2 not in ['M','m','F','f'])):
-            raise ValueError('deu erro')    
+            raise ValueError('M or F required')    
         if ((value1 == 2) and (value2 not in ['Y','y','N','n'])):
-            raise ValueError('deu erro')      
+            raise ValueError('Y or N required')      
     except ValueError as e:   
-        print(f'{e}, tente novamente') 
-
+        print(f'Invalid data: {e}, please try again.\n') 
 
 get_users_data()    
