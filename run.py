a4f1e72bdb80f0ddc46705ca7bb22c17f131d2ad
pagioro <pagioro@hotmail.com>
2022-05-03 10:36:57 +0000

    Filtering code in pep8 but still with three errors. (Too long line errors)

--- run.py
+++ run.py
@@ -20,74 +20,76 @@ print(data)
 pip3 install gspread google auth
 """
 
+
 def get_users_data():
     """
     Get data from the user.
     """
-    print('Welcome to the 2022 Census.') 
-    print('Please fill in the requested data!\n')  
+    print('Welcome to the 2022 Census.')
+    print('Please fill in the requested data!\n')
 
     while True:
-        user_name = input('Enter your name: ')        
-        if validate_data(0,user_name):            
-            break         
+        user_name = input('Enter your name: ')
+        if validate_data(0, user_name):
+            break
 
     while True:
         user_email = input('\nEnter your email: ')
-        if validate_data(1,user_email):            
-            break    
-    
+        if validate_data(1, user_email):
+            break
+
     while True:
         user_age = input('\nEnter your age: ')
-        if validate_data(2,user_age):            
-            break    
-    
+        if validate_data(2, user_age):
+            break
+
     while True:
         user_gender = input('\nEnter your gender (M or F): ')
         user_gender = user_gender.upper()
-        if validate_data(3,user_gender):            
-            break    
-    
-    user_country = input('\nEnter your country: ')    
+        if validate_data(3, user_gender):
+            break
 
-    user_city = input('\nEnter your city: ')    
+    user_country = input('\nEnter your country: ')
+
+    user_city = input('\nEnter your city: ')
 
     while True:
         user_rent = input('\nDo you pay rent? (Y or N):  ')
         user_rent = user_rent.upper()
-        if validate_data(4,user_rent):            
-            break    
-    
+        if validate_data(4, user_rent):
+            break
+
     while True:
         user_children = input('\nHow many children do you have? (0 for None):  ')
-        if validate_data(5,user_children):            
+        if validate_data(5, user_children):
             break
 
     return user_name, user_email, user_age, user_gender, user_country, user_city, user_rent, user_children
-    
-    
-def validate_data(value1,value2):
+
+
+def validate_data(value1, value2):
     try:
         """
         In the first if checks if the value2 has at least one number.
-        """        
+        """
         if ((value1 == 0) and (any(chr.isdigit() for chr in value2))):
-            raise ValueError()        
+            raise ValueError()
         if ((value1 == 1) and ('@' not in value2)):
-            raise ValueError() 
+            raise ValueError()
         if ((value1 == 2) and (int(value2) > 110)):
-            raise ValueError() 
-        if ((value1 == 3) and (value2 not in ['M','m','F','f'])):
-            raise ValueError()    
-        if ((value1 == 4) and (value2 not in ['Y','y','N','n'])):
-            raise ValueError()  
+            raise ValueError()
+        if ((value1 == 3) and (value2 not in ['M', 'm', 'F', 'f'])):
+            raise ValueError()
+        if ((value1 == 4) and (value2 not in ['Y', 'y', 'N', 'n'])):
+            raise ValueError()
         if ((value1 == 5) and (int(value2) >= 12)):
-            raise ValueError()     
-    except ValueError as e:   
+            raise ValueError()
+    except ValueError as e:
         print(f'Invalid data.\n')
         return False
 
-    return True 
+    return True
+
 
 def update_census_worksheet(data):
     """
@@ -106,17 +108,17 @@ def update_census_worksheet(data):
     if census_gender == 'F':
         census_worksheet = SHEET.worksheet("female")
         census_worksheet.append_row(data)
-        
+
     """
     Insert the male information into the male worksheet.
-    """    
+    """
     if census_gender == 'M':
         census_worksheet = SHEET.worksheet("male")
         census_worksheet.append_row(data)
 
     """
     Insert who has children information into the children worksheet.
-    """    
+    """
     census_children = SHEET.worksheet("census").cell(census_last_row, 8).value
     if int(census_children) >= 1:
         census_worksheet = SHEET.worksheet("with_children")
@@ -124,59 +126,59 @@ def update_census_worksheet(data):
 
     """
     Insert who pay rent information into the rent worksheet.
-    """    
+    """
     census_rent = SHEET.worksheet("census").cell(census_last_row, 7).value
     if census_rent == 'Y':
         census_worksheet = SHEET.worksheet("pay_rent")
         census_worksheet.append_row(data)
 
+
 def update_result_worksheet():
     """
     The number of people searched.
-    """    
+    """
     people_number = (len(SHEET.worksheet("census").get_all_values()) - 1)
     census_worksheet = SHEET.worksheet("result")
-    census_worksheet.update_cell(2, 1, people_number)    
+    census_worksheet.update_cell(2, 1, people_number)
 
     """
     The number of men.
-    """    
+    """
     men_number = (len(SHEET.worksheet("male").get_all_values()) - 1)
     census_worksheet = SHEET.worksheet("result")
-    census_worksheet.update_cell(2, 2, men_number) 
+    census_worksheet.update_cell(2, 2, men_number)
 
     """
     The number of women.
-    """    
+    """
     women_number = (len(SHEET.worksheet("female").get_all_values()) - 1)
     census_worksheet = SHEET.worksheet("result")
-    census_worksheet.update_cell(2, 3, women_number)    
+    census_worksheet.update_cell(2, 3, women_number)
 
     """
     People who pay rent.
-    """  
+    """
     rented_number = (len(SHEET.worksheet("pay_rent").get_all_values()) - 1)
     census_worksheet = SHEET.worksheet("result")
-    census_worksheet.update_cell(2, 4, rented_number)  
+    census_worksheet.update_cell(2, 4, rented_number)
 
     """
-    People who own their own homes.    
-    """   
+    People who own their own homes.
+    """
     owner_number = 0
-    owner_number += (people_number - rented_number)  
+    owner_number += (people_number - rented_number)
     census_worksheet = SHEET.worksheet("result")
     census_worksheet.update_cell(2, 5, owner_number)
 
     """
     How many people have children?
-    """   
+    """
     children_number = (len(SHEET.worksheet("with_children").get_all_values()) - 1)
     census_worksheet = SHEET.worksheet("result")
-    census_worksheet.update_cell(2, 6, children_number)  
-
+    census_worksheet.update_cell(2, 6, children_number)
     """
     Result
-    """  
+    """
     print(f'The number of people searched: {people_number} \n')
     print(f'The number of men: {men_number} \n')
     print(f'The number of women: {women_number} \n')
@@ -184,12 +186,12 @@ def update_result_worksheet():
     print(f'People who own their own homes: {owner_number} \n')
     print(f'How many people have children?: {children_number} \n')
 
-
     """
     Tem mais de 3 filhos
-    children_last_row = (len(SHEET.worksheet("with_children").get_all_values()) - 1)
+    children_last_row = (len(SHEET.worksheet("with_children")
+                        .get_all_values()) - 1)
     children_row = 2
-    children_number = 0 
+    children_number = 0
     children = SHEET.worksheet("with_children").cell(children_row, 8).value
 
     while children_row <= children_last_row:
@@ -197,16 +199,14 @@ def update_result_worksheet():
             children_number += 1
         children_row += 1
         children = SHEET.worksheet("with_children").cell(children_row, 8).value
-       
-    census_worksheet = SHEET.worksheet("result")
-    census_worksheet.update_cell(2, 6, children_number)  
 
-    """      
- 
+    census_worksheet = SHEET.worksheet("result")
+    census_worksheet.update_cell(2, 6, children_number)
+    """
+
 
 def main():
     data = get_users_data()
     update_census_worksheet(data)
     update_result_worksheet()
-
 main()
